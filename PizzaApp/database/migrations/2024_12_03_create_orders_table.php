ee1f16e860013233c400c44572e4c768a8f10168
Ossama <130113024+yssokach@users.noreply.github.com>
2024-12-07 17:23:54 +0100

    Create 2024_12_03_create_orders_table.php

---
+++ PizzaApp/database/migrations/2024_12_03_create_orders_table.php
@@ -0,0 +1,26 @@
+<?php
+
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->float('total_amount');
+            $table->float('total_cost');
+            $table->float('total_profit');
+            $table->enum('status', ['pending', 'completed', 'cancelled'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
